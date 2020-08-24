a5e68ee9fb0bee553fc7bb0270d350a4f641f839
uensung <31657365+uensung@users.noreply.github.com>
2020-08-24 22:24:53 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var filter = "win16|win32|win64|mac|macintel";
 if ( navigator.platform ) {
   if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) {
-    alert('모바일');
+    window.location.href = "/m";
   }
   else {
-    alert('PC');
+    window.location.href = "/";
   }
 }
 
