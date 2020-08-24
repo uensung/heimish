2837b0efec4ff05c0ffa8eafbe49dd0bc8574ff8
uensung <31657365+uensung@users.noreply.github.com>
2020-08-24 23:11:58 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var filter = "win16|win32|win64|mac|macintel";
 if ( navigator.platform ) {
   if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) {
+    alert('m');
     window.location.replace = "/heimish/m";
   }
   else {
+    alert('pc');
     window.location.replace = "/heimish";
   }
 }
