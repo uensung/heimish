4b862a0815eb13776f103f51b9c7fa9f66bcba72
uensung <31657365+uensung@users.noreply.github.com>
2020-08-25 11:05:45 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var filter = "win16|win32|win64|mac|macintel";
 if ( navigator.platform ) {
   if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) {
-    alert('m');
     parent.window.location.href = "https://uensung.github.io/heimish/m/";
   }
 }
