62854e43e5fe97524e142a6f1e135e25debe14ca
uensung <31657365+uensung@users.noreply.github.com>
2020-08-25 09:56:57 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ var filter = "win16|win32|win64|mac|macintel";
 if ( navigator.platform ) {
   if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) {
     alert('m');
-    window.location.replace = "https://uensung.github.io/heimish/m/";
+    location.replace = "https://uensung.github.io/heimish/m/";
   }
   else {
     alert('pc');
-    window.location.replace = "https://uensung.github.io/heimish/";
+    location.replace = "https://uensung.github.io/heimish/";
   }
 }
 
